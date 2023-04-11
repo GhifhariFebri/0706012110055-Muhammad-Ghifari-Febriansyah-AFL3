2982e167c83f1a67062e9d96b0fce51194f40c2e
GhifhariFebri <suriansyahghifari@gmail.com>
2023-04-11 08:39:14 +0700

    Section 2 Commit

--- 0706012110055-Muhammad Ghifari Febriansyah-AFL3/ContentView.swift
+++ 0706012110055-Muhammad Ghifari Febriansyah-AFL3/ContentView.swift
@@ -10,10 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, Ghifari!")
+            Text("Turtle Rock")
+                .font(.title)
+                .foregroundColor(.black)
         }
         .padding()
     }
